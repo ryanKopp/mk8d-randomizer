076313d4686e29576e1d454eee0ed2df8c70f6c1
Ryan Kopp <ryck574@gmail.com>
2023-01-14 23:04:16 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+use core::fmt;
+use std::error::Error;
+//use std::process;
+use std::fs::File;
+use serde::Deserialize;
+use rand::seq::IteratorRandom;
+use std::ops;
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct Statstick {
+    name: String,
+    speed : f32,
+    acceleration: f32,
+    weight : f32,
+    traction: f32, 
+    handling: f32,
+}
+
+impl ops::Add<Statstick> for Statstick {
+    type Output = Statstick;
+
+    fn add(mut self, _rhs: Statstick) -> Statstick {
+        self.name = format!("{}, {}", self.name, _rhs.name); 
+        self.speed += _rhs.speed;
+        self.acceleration += _rhs.acceleration;
+        self.weight += _rhs.weight;
+        self.traction += _rhs.traction;
+        self.handling += _rhs.handling;
+        
+        self
+    }
+}
+
+impl fmt::Display for Statstick {
+    fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}\n", self.name)?;
+        write!(f, "Speed: {}\n{}\n", self.speed, generate_bar(self.speed))?;
+        write!(f, "Acceleration: {}\n{}\n", self.acceleration, generate_bar(self.acceleration))?;
+        write!(f, "Weight: {}\n{}\n", self.weight, generate_bar(self.weight))?;
+        write!(f, "Traction: {}\n{}\n", self.traction, generate_bar(self.traction))?;
+        write!(f, "Handling: {}\n{}\n", self.handling, generate_bar(self.handling))
+    }
+}
+
+fn generate_bar(num: f32) -> String {
+    let bar_width = 29;
+    let num_pounds: u8 = (num * 4.0) as u8;
+    let mut bar = String::from("[");
+
+    for i in 1..bar_width{
+        if i % 5 == 0 {
+            bar += "|";
+        }else if i <= num_pounds + i/5 {
+            bar += "*";
+        }
+        else{
+            bar += " ";
+        }
+    }
+    bar += "]";
+
+    bar
+}
+
+
+fn read_csv(fname: &str) -> Result<Statstick, Box<dyn Error>> {
+
+    let file = File::open(fname)?;
+    let mut rdr = csv::Reader::from_reader(file);
+
+    // Pick a random record to deserialize into struct
+    let record: Statstick = rdr.deserialize()
+        .choose(&mut rand::thread_rng())
+        .unwrap().unwrap();
+    Ok(record)
+}
+
+fn main() {
+    let driver = read_csv("csv/DRIVERS.csv").expect("failure");
+    let kart = read_csv("csv/VEHICLES.csv").expect("failure");
+    let tire = read_csv("csv/TIRES.csv").expect("failure");
+    let glider = read_csv("csv/GLIDERS.csv").expect("failure");
+
+    let combo = driver + kart + tire + glider;
+    println!("{}", combo);
+
+}
