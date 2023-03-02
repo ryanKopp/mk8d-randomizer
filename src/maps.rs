3a539c50c416f16a20ef6a98a08d3d3b04516a84
Ryan Kopp <ryck574@gmail.com>
2023-03-01 22:57:08 -0500

    Split code into smaller files

---
+++ src/maps.rs
@@ -0,0 +1,20 @@
+use crate::data;
+use rand::seq::SliceRandom;
+
+
+pub fn get_map_list() -> Vec<String> { 
+    let mut rdr = csv::Reader::from_reader(data::MAPS.as_bytes());
+    let mut maps = Vec::new();
+
+    for record in rdr.records(){
+        let result = record.unwrap();
+        maps.push(result.as_slice().to_string());
+    }
+    return maps;
+}
+
+pub fn shuffle_maps(maps: &mut Vec<String>) {
+    
+    let mut rng = rand::thread_rng();
+    maps.shuffle(&mut rng);
+}
