c3dae7a598c9045fbe647580a214482574d3d9c0
Ryan Kopp <ryck574@gmail.com>
2023-11-11 13:24:42 -0700

    Remove unused map code

--- src/maps.rs
+++ src/maps.rs
@@ -1,7 +1,6 @@
 use crate::data;
 use rand::seq::SliceRandom;
 
-#[allow(dead_code)]
 pub fn get_map_list() -> Vec<String> {
     let mut rdr = csv::Reader::from_reader(data::MAPS.as_bytes());
     let mut maps = Vec::new();
@@ -18,13 +17,4 @@ pub fn get_map_list() -> Vec<String> {
 pub fn shuffle_maps(maps: &mut Vec<String>) {
     let mut rng = rand::thread_rng();
     maps.shuffle(&mut rng);
-}
-
-#[allow(dead_code)]
-pub fn take_maps(map_num: u32) -> String {
-    get_map_list()
-        .iter()
-        .take(map_num.try_into().unwrap())
-        .map(|x| x.to_string() + "\n")
-        .collect::<String>()
 }
